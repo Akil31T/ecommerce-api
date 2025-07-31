da40f69f0f29ba7d5e04c45231560c9b8a676ded
Akil <akil31082000@gmail.com>
2025-07-31 13:23:01 +0530

    Refactor: Update MongoDB connection handling and modify Vercel routing

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
-import app from "./app"
-import config from "./app/config"
+import app from "./app";
+import config from "./app/config";
 
-async function main() {
-  await mongoose.connect(config.db_url as string);
-app.listen(config.port, () => {
-  console.log(`Example app listening on port ${config.port}`)
-})
-}
+mongoose
+  .connect(config.db_url as string)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB connection error:", error);
+  });
 
-main().then(()=>console.log('mongodb is connected')).catch(error=>console.log(error))
+module.exports = app; 
