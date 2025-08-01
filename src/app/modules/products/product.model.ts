0f0b30a1ad779de4bbb57272de79ca35148b74ac
Akil <akil31082000@gmail.com>
2025-08-01 14:40:02 +0530

    Add: Log success message after product creation and clean up product model schema

--- src/app/modules/products/product.model.ts
+++ src/app/modules/products/product.model.ts
@@ -2,22 +2,19 @@ import { model, Schema } from 'mongoose';
 import { TInventory, TProduct, TVariant } from './product.interface';
 
 const VariantSchema = new Schema<TVariant>({
-    type: String, 
+    type: String,
     value: String
-}, {_id: false})
+}, { _id: false })
 
 
 const InventorySchema = new Schema<TInventory>({
     quantity: Number,
     inStock: Boolean
-}, {_id: false})
+}, { _id: false })
 
 const ProductSchema = new Schema<TProduct>({
-    name : {
-        type: String,
-        required: true,
-    },
-    image:String,
+    name: String,
+    image: String,
     description: String,
     price: Number,
     category: String,
